6b9c7d3ba208712d6d8549d1a7805f1065a741f0
German Yanovich <germanyanovich@gmail.com>
2020-05-24 18:41:19 +0300

    add fade slider

--- src/js/sidebar-menu.js
+++ src/js/sidebar-menu.js
@@ -1,11 +1,13 @@
-const openBtn = document.querySelector('.sidebar__hamburger');
-const closeBtn = document.querySelector('.sidebar__close');
-const sidebar = document.querySelector('.sidebar');
+(function () {
+  const openBtn = document.querySelector('.sidebar__hamburger');
+  const closeBtn = document.querySelector('.sidebar__close');
+  const sidebar = document.querySelector('.sidebar');
 
-openBtn.addEventListener('click', () => {
-  sidebar.classList.add('sidebar--opened');
-});
+  openBtn.addEventListener('click', () => {
+    sidebar.classList.add('sidebar--opened');
+  });
 
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.remove('sidebar--opened');
-});
+  closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar--opened');
+  });
+})();
